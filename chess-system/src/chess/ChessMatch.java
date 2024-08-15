8411bf055ef6c4575ae481cba585174f8841f972
Matheus Gomes <matheus.recin@outlook.com>
2024-08-15 16:10:27 -0300

    BoardingException e programação defensiva

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -15,9 +15,9 @@ public class ChessMatch {
     }
 
     public ChessPiece[][] getPieces(){
-        ChessPiece[][] matrizAux = new ChessPiece[board.getRows()][board.getColumn()];
+        ChessPiece[][] matrizAux = new ChessPiece[board.getRows()][board.getColumns()];
         for(int i=0; i< board.getRows(); i++){
-            for (int j=0; j< board.getColumn(); j++){
+            for (int j = 0; j< board.getColumns(); j++){
                 matrizAux[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
@@ -27,7 +27,7 @@ public class ChessMatch {
     private void initialSetup(){
         board.placePiece(new Torre(board, Color.WHITE), new Position(2,1));
         board.placePiece(new Rei(board, Color.BLACK), new Position(0, 2));
-        board.placePiece(new Rei(board, Color.WHITE), new Position(0, 4));
+        board.placePiece(new Rei(board, Color.WHITE), new Position(5, 4));
 
 
 
