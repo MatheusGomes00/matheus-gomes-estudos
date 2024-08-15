35defec4d916bd1ac491589ed0cd93f866f9cfcd
Matheus Gomes <matheus.recin@outlook.com>
2024-08-15 17:21:02 -0300

    Movendo peças

--- chess-system/src/application/App.java
+++ chess-system/src/application/App.java
@@ -3,12 +3,28 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while(true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.print("\nOrigem: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.print("\nDestino: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
 
 
     }
