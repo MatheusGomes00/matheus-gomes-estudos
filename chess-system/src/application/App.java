00ae23ab9986442beda24cbf13db70ad6bc2cd4c
Matheus Gomes <matheus.recin@outlook.com>
2024-08-16 10:37:20 -0300

    Tratamento de exceções e limpeza da tela

--- chess-system/src/application/App.java
+++ chess-system/src/application/App.java
@@ -2,30 +2,42 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
+        // para acessar via terminal, de preferencia o bash do git
+        // acessar o diretorio do projeto:
+        // C:\Projetos\DesafioXadrez\out\production\chess-system
+        // e rodar o comando: java application.app
+
         Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
 
-        while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.print("\nOrigem: ");
-            ChessPosition source = UI.readChessPosition(sc);
+        while(true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.print("\nOrigem: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.print("\nDestino: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.print("\nDestino: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch(ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
-
 
     }
 }
