6d7e432cf791a0a8ac0151c3bd77dc8385906b51
Matheus Gomes <matheus.recin@outlook.com>
2024-08-22 22:28:33 -0300

    feat: Department entity classes

--- Semana-03/exercicios/codeWithJDBCandMysql/projetoDemoDaoJDBC/demo-dao-jdbc/src/application/App.java
+++ Semana-03/exercicios/codeWithJDBCandMysql/projetoDemoDaoJDBC/demo-dao-jdbc/src/application/App.java
@@ -1,7 +1,11 @@
 package application;
 
+import entities.Department;
+
 public class App {
     public static void main(String[] args) {
 
+        Department obj = new Department(1, "Eletronics");
+        System.out.println(obj);
     }
 }
