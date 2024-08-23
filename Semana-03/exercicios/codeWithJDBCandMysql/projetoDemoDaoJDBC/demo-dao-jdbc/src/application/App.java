3e921d48e0c31b952d56817879f5fc91a9fb1ba0
Matheus Gomes <matheus.recin@outlook.com>
2024-08-22 22:34:11 -0300

    feat: Seller entity classes

--- Semana-03/exercicios/codeWithJDBCandMysql/projetoDemoDaoJDBC/demo-dao-jdbc/src/application/App.java
+++ Semana-03/exercicios/codeWithJDBCandMysql/projetoDemoDaoJDBC/demo-dao-jdbc/src/application/App.java
@@ -1,11 +1,19 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Eletronics");
-        System.out.println(obj);
+
+        Seller seller = new Seller(10, "Matias", "matias@gmail.com", new Date(), 5000.0, obj);
+
+        System.out.println(seller);
+
+
     }
 }
