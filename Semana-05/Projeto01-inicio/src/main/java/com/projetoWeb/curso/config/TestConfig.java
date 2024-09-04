492ca9c769911136df1b521d274bbcf3aa5703bc
Matheus Gomes <matheus.recin@outlook.com>
2024-09-04 16:00:29 -0300

    feat: Category

--- Semana-05/Projeto01-inicio/src/main/java/com/projetoWeb/curso/config/TestConfig.java
+++ Semana-05/Projeto01-inicio/src/main/java/com/projetoWeb/curso/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.projetoWeb.curso.config;
 
+import com.projetoWeb.curso.model.entities.Category;
 import com.projetoWeb.curso.model.entities.Order;
 import com.projetoWeb.curso.model.entities.User;
 import com.projetoWeb.curso.model.entities.enums.OrderStatus;
+import com.projetoWeb.curso.repositories.CategoryRepository;
 import com.projetoWeb.curso.repositories.OrderRepository;
 import com.projetoWeb.curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,10 +30,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ Semana-05/Projeto01-inicio/src/main/java/com/projetoWeb/curso/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.projetoWeb.curso.services;
+
+import com.projetoWeb.curso.model.entities.Category;
+import com.projetoWeb.curso.model.entities.User;
+import com.projetoWeb.curso.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
