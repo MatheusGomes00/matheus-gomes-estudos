b8d8c19f609cf75700ef2a73cd46a5a2c71f5d65
Matheus Gomes <matheus.recin@outlook.com>
2024-09-11 16:19:39 -0300

    feat: CreateUser method POST

--- Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/service/UsuarioService.java
+++ Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.pbCompass.parkApi.service;
 
+import com.pbCompass.parkApi.entity.Usuario;
 import com.pbCompass.parkApi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor  // inicia a injeção de dependencia via construtor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/web/controller/UsuarioController.java
+++ Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/web/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package com.pbCompass.parkApi.web.controller;
 
 
+import com.pbCompass.parkApi.entity.Usuario;
 import com.pbCompass.parkApi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario newUser = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
 }
