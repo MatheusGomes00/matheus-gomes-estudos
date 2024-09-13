88d440830d10d93aa5b3d2149f6d5250319d1784
Matheus Gomes <matheus.recin@outlook.com>
2024-09-12 23:08:10 -0300

    feat: documentação para buscar atualizar senha

--- Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/web/controller/UsuarioController.java
+++ Semana-06/ParkApi/parkApi/src/main/java/com/pbCompass/parkApi/web/controller/UsuarioController.java
@@ -59,6 +59,16 @@ public class UsuarioController {
         return ResponseEntity.ok(UsuarioMapper.toDto(newUser));  // status code 200
     }
 
+    @Operation(summary = "Atualizar senha", description = "Atualizar senha",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "400", description = "Senha não confere",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto senhaDto){
         Usuario newUser = usuarioService.editarSenha(id, senhaDto.getSenhaAtual(), senhaDto.getNovaSenha(), senhaDto.getConfirmaSenha());
