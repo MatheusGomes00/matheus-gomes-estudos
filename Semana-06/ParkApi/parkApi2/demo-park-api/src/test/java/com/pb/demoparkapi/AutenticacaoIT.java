d66c938871c1fe36b9aaac6f8cfc93c13356ca8b
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2024-09-17 21:37:16 -0300

    test: autenticação com status code 200

---
+++ Semana-06/ParkApi/parkApi2/demo-park-api/src/test/java/com/pb/demoparkapi/AutenticacaoIT.java
@@ -0,0 +1,35 @@
+package com.pb.demoparkapi;
+
+import com.pb.demoparkapi.jwt.JwtToken;
+import com.pb.demoparkapi.web.dto.UsuarioLoginDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)  // spring gerencia os testes em um servidor atraves de uma porta randomica os testes
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)  // script utilizados para popular o banco de dados
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)  // script utilizados para popular o banco de dados
+public class AutenticacaoIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void autenticar_ComCredenciaisValidas_RetornarTokenComStatus200(){
+        JwtToken responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        
+    }
+}
