157b2dbdf365b72b71ece7deed2c42a0a19bcfc5
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2024-09-18 20:09:42 -0300

    test: teste ponto a ponto para Create status 201

---
+++ Semana-06/ParkApi/parkApi2/demo-park-api/src/test/java/com/pb/demoparkapi/ClienteIT.java
@@ -0,0 +1,38 @@
+package com.pb.demoparkapi;
+
+import com.pb.demoparkapi.web.dto.ClienteCreateDto;
+import com.pb.demoparkapi.web.dto.ClienteResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ClienteIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void criarCliente_ComDadosValidos_RetornarClienteComStatus201() {
+        ClienteResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "matias@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Matias Hefrayn", "50983575002"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ClienteResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNome()).isEqualTo("Matias Hefrayn");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("50983575002");
+    }
+}
