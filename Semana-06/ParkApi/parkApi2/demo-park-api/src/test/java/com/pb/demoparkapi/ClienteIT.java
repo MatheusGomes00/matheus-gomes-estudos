8caf412a4ec2014e33cebb309b6e628064709098
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2024-09-18 20:38:28 -0300

    test: teste ponto a ponto para Create status 403, 409 e 422

--- Semana-06/ParkApi/parkApi2/demo-park-api/src/test/java/com/pb/demoparkapi/ClienteIT.java
+++ Semana-06/ParkApi/parkApi2/demo-park-api/src/test/java/com/pb/demoparkapi/ClienteIT.java
@@ -2,6 +2,7 @@ package com.pb.demoparkapi;
 
 import com.pb.demoparkapi.web.dto.ClienteCreateDto;
 import com.pb.demoparkapi.web.dto.ClienteResponseDto;
+import com.pb.demoparkapi.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,4 +36,84 @@ public class ClienteIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getNome()).isEqualTo("Matias Hefrayn");
         org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("50983575002");
     }
+
+    @Test
+    public void criarCliente_ComCpfJaCadastrado_RetornarErrorMessageStatus409() {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "matias@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Matias Hefrayn", "79074426050"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
+
+    @Test
+    public void criarCliente_ComDadosInvalidos_RetornarErrorMessageStatus422() {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "matias@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("", ""))  // dados nulos
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "matias@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Judas", "00000000000")) // cpf invalido
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "matias@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Bobb", "911.910.640-85"))  // cpf com mais de 11 caracteres
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void criarCliente_ComUsuarioNaoPermitido_RetornarErrorMessageStatus403() {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "pedro@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Pedro Gordo", "91191064085"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+    }
+
 }
--- Semana-06/ParkApi/parkApi2/demo-park-api/src/test/resources/sql/clientes/clientes-insert.sql
+++ Semana-06/ParkApi/parkApi2/demo-park-api/src/test/resources/sql/clientes/clientes-insert.sql
@@ -1,7 +1,7 @@
 insert into USUARIOS (id, username, password, role) values (100, 'pedro@email.com', '$2a$12$TBD0hbc0opCmOptBxjDoMuBb8PScsRU74JGfWgJX3YDOIy47Te9P2', 'ROLE_ADMIN');
 insert into USUARIOS (id, username, password, role) values (101, 'julia@email.com', '$2a$12$TBD0hbc0opCmOptBxjDoMuBb8PScsRU74JGfWgJX3YDOIy47Te9P2', 'ROLE_CLIENTE');
 insert into USUARIOS (id, username, password, role) values (102, 'ana@email.com', '$2a$12$TBD0hbc0opCmOptBxjDoMuBb8PScsRU74JGfWgJX3YDOIy47Te9P2', 'ROLE_CLIENTE');
-insert into USUARIOS (id, username, password, role) values (103, 'matias@email.com', '$2a$12$VQSVztr/M8tcwzcy9jJgFebpqybKpF4FVwM3zilhOAI4yL1iW3rJa', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (103, 'matias@email.com', '$2a$12$TBD0hbc0opCmOptBxjDoMuBb8PScsRU74JGfWgJX3YDOIy47Te9P2', 'ROLE_CLIENTE');
 
 insert into CLIENTES (id, nome, cpf, id_usuario) values (10, 'Julia Romana', '79074426050', 101);
 insert into CLIENTES (id, nome, cpf, id_usuario) values (20, 'Ana Cherosa', '55352517047', 102);
